7698f8094fc95d01e99b2079813eb04b927d6b5b
Tokov <flyingcatsad@gmail.com>
2021-10-15 11:48:55 +0300

    Add buttons

---
+++ src/components/Buttons/Buttons.jsx
@@ -0,0 +1,16 @@
+import "./Buttons.css";
+
+function Buttons() {
+  return (
+    <div className="buttons">
+      <a href="/" className="button message">
+        Message
+      </a>
+      <a href="/" className="button follow">
+        Following
+      </a>
+    </div>
+  );
+}
+
+export default Buttons;
